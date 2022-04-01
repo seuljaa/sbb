823744fe4a400435ef9c3084f94b776d083dd3d5
seulja <seuljaa940503@gmail.com>
2022-04-01 13:46:04 +0900

    질문 리스트 출력

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,32 @@
 package com.mysite.sbb;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
 
 @SpringBootTest
 class SbbApplicationTests {
-
+	
+	@Autowired
+	private QuestionRepository questionRepository;
+	
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("제목1입니다.");
+		q1.setContent("내용1입니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);
+		
+		Question q2 = new Question();
+		q2.setSubject("제목2입니다.");
+		q2.setContent("내용2입니다.");
+		q2.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);
 	}
-
 }
