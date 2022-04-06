82a804f188becceae072a465763acdcddea80b7f
seulja <seuljaa940503@gmail.com>
2022-04-06 11:50:09 +0900

    질문작성시 null 허용안함, null시 다른항목의 값은 유지한채로 오류메시지 출력

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -2,8 +2,11 @@ package com.mysite.sbb.question;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,13 +37,16 @@ public class QuestionController {
 	}
 	
 	@GetMapping("/create")
-	public String questionCreate() {
+	public String questionCreate(QuestionForm questionForm) {
 		return "question_form";
 	}
 	
 	@PostMapping("/create")
-	public String questionCreate(@RequestParam String subject, @RequestParam String content) {
-		this.questionService.create(subject, content);
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent());
 		return "redirect:/question/list";
 	}
 	
